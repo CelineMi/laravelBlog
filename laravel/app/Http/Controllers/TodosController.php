d621e2f082bd49e635ef49b60cd92c0508804080
Céline CMR. MINAUDIER <CMR@genesis-groupe.com>
2019-10-04 15:56:10 +0200

    initial commit

---
+++ laravel/app/Http/Controllers/TodosController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index ()
+    {
+//        fetch all todos from DB
+        $todos = Todo::all();
+//        display them in the todos/index page
+
+        return view('todos.index')->with('todos', $todos);
+    }
+}
